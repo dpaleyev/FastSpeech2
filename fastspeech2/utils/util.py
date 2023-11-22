1499705cc59fd1f168a67ea4b6cce54a6158b58a
Daniel Paleyev <danny.paleyev@gmail.com>
2023-11-22 18:36:02 +0300

    collator

--- fastspeech2/utils/util.py
+++ fastspeech2/utils/util.py
@@ -2,7 +2,9 @@ import json
 from collections import OrderedDict
 from itertools import repeat
 from pathlib import Path
+import torch.nn.functional as F
 
+import numpy as np
 import pandas as pd
 import torch
 
@@ -88,3 +90,91 @@ def process_text(train_text_path):
             txt.append(line)
 
         return txt
+
+def pad_1D(inputs, PAD=0):
+
+    def pad_data(x, length, PAD):
+        x_padded = np.pad(x, (0, length - x.shape[0]),
+                          mode='constant',
+                          constant_values=PAD)
+        return x_padded
+
+    max_len = max((len(x) for x in inputs))
+    padded = np.stack([pad_data(x, max_len, PAD) for x in inputs])
+
+    return padded
+
+
+def pad_1D_tensor(inputs, PAD=0):
+
+    def pad_data(x, length, PAD):
+        x_padded = F.pad(x, (0, length - x.shape[0]))
+        return x_padded
+
+    max_len = max((len(x) for x in inputs))
+    padded = torch.stack([pad_data(x, max_len, PAD) for x in inputs])
+
+    return padded
+
+
+def pad_2D(inputs, maxlen=None):
+
+    def pad(x, max_len):
+        PAD = 0
+        if np.shape(x)[0] > max_len:
+            raise ValueError("not max_len")
+
+        s = np.shape(x)[1]
+        x_padded = np.pad(x, (0, max_len - np.shape(x)[0]),
+                          mode='constant',
+                          constant_values=PAD)
+        return x_padded[:, :s]
+
+    if maxlen:
+        output = np.stack([pad(x, maxlen) for x in inputs])
+    else:
+        max_len = max(np.shape(x)[0] for x in inputs)
+        output = np.stack([pad(x, max_len) for x in inputs])
+
+    return output
+
+
+def pad_2D_tensor(inputs, maxlen=None):
+
+    def pad(x, max_len):
+        if x.size(0) > max_len:
+            raise ValueError("not max_len")
+
+        s = x.size(1)
+        x_padded = F.pad(x, (0, 0, 0, max_len-x.size(0)))
+        return x_padded[:, :s]
+
+    if maxlen:
+        output = torch.stack([pad(x, maxlen) for x in inputs])
+    else:
+        max_len = max(x.size(0) for x in inputs)
+        output = torch.stack([pad(x, max_len) for x in inputs])
+
+    return output
+
+
+def pad(input_ele, mel_max_length=None):
+    if mel_max_length:
+        out_list = list()
+        max_len = mel_max_length
+        for i, batch in enumerate(input_ele):
+            one_batch_padded = F.pad(
+                batch, (0, 0, 0, max_len-batch.size(0)), "constant", 0.0)
+            out_list.append(one_batch_padded)
+        out_padded = torch.stack(out_list)
+        return out_padded
+    else:
+        out_list = list()
+        max_len = max([input_ele[i].size(0)for i in range(len(input_ele))])
+
+        for i, batch in enumerate(input_ele):
+            one_batch_padded = F.pad(
+                batch, (0, 0, 0, max_len-batch.size(0)), "constant", 0.0)
+            out_list.append(one_batch_padded)
+        out_padded = torch.stack(out_list)
+        return out_padded
